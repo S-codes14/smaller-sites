06641754e6a63199fcdca0700a34d12d8913bc43
S-codes14 <smlmnguni14@gmail.com>
2021-07-04 03:34:43 +0200

    electron works

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ let win
 function createWindow() { 
    win = new BrowserWindow({width: 800, height: 600}) 
    win.loadURL(url.format ({ 
-      pathname: path.join(__dirname, '/app/index.html'), 
+      // pathname: path.join(__dirname, '/app/index.html'),
+      pathname: path.join(__dirname, '/index.html'), 
       protocol: 'file:', 
       slashes: true 
    })) 
